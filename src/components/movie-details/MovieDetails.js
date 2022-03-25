c2ed5fc8d436a417f40c70e65aa1a160734f630f
Sirius <30043962+siriusnottin@users.noreply.github.com>
2022-03-25 23:43:09 +0100

    feat: Movie App

---
+++ src/components/movie-details/MovieDetails.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class MovieDetails extends Component {
+	render() {
+		return (
+			<aside className="w-25 border p-4 d-flex flex-column">
+				<h2>{this.props.movie.title}</h2>
+				<hr className="w-100" />
+				<div>
+					<img
+						className="d-block mx-auto"
+						src={this.props.movie.img}
+						alt={'Affiche du film ' + this.props.movie.title}
+					/>
+				</div>
+				<hr className="w-100" />
+				<p className="text-secondary">{this.props.movie.details}</p>
+				<p className="mb-0">{this.props.movie.description}</p>
+			</aside>
+		);
+	}
+}
---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import MovieElement from './MovieElement';
+
+export default class MovieList extends Component {
+	render() {
+		return (
+			<main className="w-75 d-flex flex-row flex-wrap align-content-start">
+				<MovieElement
+					movie={this.props.movies[0]}
+					updateSelectedMovie={this.props.updateSelectedMovie}
+				/>
+				<MovieElement
+					movie={this.props.movies[1]}
+					updateSelectedMovie={this.props.updateSelectedMovie}
+				/>
+				<MovieElement
+					movie={this.props.movies[2]}
+					updateSelectedMovie={this.props.updateSelectedMovie}
+				/>
+				<MovieElement
+					movie={this.props.movies[3]}
+					updateSelectedMovie={this.props.updateSelectedMovie}
+				/>
+			</main>
+		);
+	}
+}
