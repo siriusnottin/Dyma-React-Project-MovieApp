51f59d90f7375e1bd78726a2854c916fc6186d1f
Sirius <30043962+siriusnottin@users.noreply.github.com>
2022-03-28 14:49:06 +0200

    styles: formatted with Prettier

--- src/config/api.movie.js
+++ src/config/api.movie.js
@@ -1,21 +1,20 @@
 import * as axios from 'axios';
 
 const apiMovie = axios.create({
-
-    baseURL: 'https://api.themoviedb.org/3/',
-
+  baseURL: 'https://api.themoviedb.org/3/',
 });
 
-apiMovie.interceptors.request.use( req => {
-    req.headers['Authorization'] = "Bearer " + process.env.REACT_APP_TMDB_API_TOKEN;
-    return req;
+apiMovie.interceptors.request.use((req) => {
+  req.headers['Authorization'] =
+    'Bearer ' + process.env.REACT_APP_TMDB_API_TOKEN;
+  return req;
 });
 
 export default apiMovie;
 
 export const apiMovieMap = (movie) => ({
-    title: movie.title,
-    img: 'https://image.tmdb.org/t/p/w500' + movie.poster_path,
-    details: `${movie.release_date} | ${movie.vote_average} / 10 (${movie.vote_count} votes)`,
-    description: movie.overview
-})
+  title: movie.title,
+  img: 'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+  details: `${movie.release_date} | ${movie.vote_average} / 10 (${movie.vote_count} votes)`,
+  description: movie.overview,
+});
